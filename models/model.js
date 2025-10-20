f4e75cd541fe419c89902db7da183b3124c70ff3
jeserobertosl-ui <jeserobertosl@gmail.com>
2025-10-20 09:48:33 -0600

    Added sorting and filtering
    routes now have functions to handle adding comments and grades and filtering restaurants

--- models/model.js
+++ models/model.js
@@ -1,14 +1,66 @@
 const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
+    address: {
+        required: true,
+        type: {
+            building: {
+                required: true,
+                type: String
+            },
+            coord: {
+                required: true,
+                type: [Number]
+            },
+            street: {
+                required: true,
+                type: String
+            },
+            zipcode: {
+                required: true,
+                type: String
+            }
+        }
+    },
+    borough: {
+        required: true,
+        type: String
+    },
+    cuisine: {
+        required: true,
+        type: String
+    },
+    grades: {
+        required: true,
+        type: [{
+            date: {
+                required: true,
+                type: Date
+            },
+            grade: {
+                required: true,
+                type: String
+            },
+            score: {
+                required: true,
+                type: Number
+            }
+        }]
+    },
     name: {
         required: true,
         type: String
     },
-    age: {
+    restaurant_id: {
         required: true,
-        type: Number
+        type: String
+    },
+    comments: {
+        type: [{
+            required:true,
+            type: String
+        }]
     }
-})
+}, {collection: "restaurants"})
 
 module.exports = mongoose.model('Data', dataSchema)
